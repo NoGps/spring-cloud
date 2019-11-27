45ec129aa0f6ae2d21673befbee78b3031d76b3f
xiayue <xiayue@casachina.com.cn>
2019-11-27 10:07:15 +0800

    creat project

---
+++ src/main/java/pers/xiayue/studycloud/StudycloudApplication.java
@@ -0,0 +1,13 @@
+package pers.xiayue.studycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudycloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/xiayue/studycloud/StudycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.xiayue.studycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
